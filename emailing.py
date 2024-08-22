f2afadf0393e2a0bbf03c65873ba83bb2c3ba865
valko <valkoista49@gmail.com>
2024-08-22 11:43:47 +0200

    Add send email function

--- emailing.py
+++ emailing.py
@@ -1,2 +1,28 @@
-def send_email():
-    print("Email was sent")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+USERNAME = 'hefano.mne@gmail.com'
+PASSWORD = 'kgurxznhhdvkbujx'
+
+
+def send_email(image_path):
+    print('send started')
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer!")
+    with open(image_path, 'rb') as file:
+        content = file.read()
+        email_message.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(USERNAME, PASSWORD)
+    gmail.sendmail(USERNAME, USERNAME, email_message.as_string())
+    gmail.quit()
+    print('send finished')
+
+
+if __name__ == "__main__":
+    send_email(image_path='images/40.png')
